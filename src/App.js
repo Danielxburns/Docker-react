847e9beed4f1aa9eab5e083c98fb0b48284b7cc5
Danielxburns <41316262+Danielxburns@users.noreply.github.com>
2019-11-25 22:06:41 -0600

    updated landing page

--- src/App.js
+++ src/App.js
@@ -6,22 +6,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi. How are you?
-        </p>
-        <p id="frog">O{'   '}O<br />
-         \{ }/<br />
-        O<br />
-        /_\\/_</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p id="frog">
+          O&nbsp;&nbsp;&nbsp;O<br />
+          \&nbsp;/<br />
+          O<br />
+          /_\\/_</p>
+        <p>Hi. How are you?</p>
       </header>
     </div>
   );
